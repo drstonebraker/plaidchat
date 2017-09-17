700a184b339afae538e34cc58748786224bf01b7
drstonebraker <drew.stonebraker@gmail.com>
2017-09-16 22:23:24 -0700

    Add npm packages and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+  entry: './frontend/entry.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ],
+    plugins: [
+      new LiveReloadPlugin({})
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
