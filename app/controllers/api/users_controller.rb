a88998f88b2ce76f115e104ae3898ad8fcee475e
drstonebraker <drew.stonebraker@gmail.com>
2017-09-20 15:37:37 -0700

    Generate user auth model and controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,10 @@
+class Api::UsersController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/root'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create, :update]
+    resource :session, only: [:create, :destroy]
+  end
 
   root "static_pages#root"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
