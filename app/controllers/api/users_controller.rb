903bf6c000ecc10d8c5187384b21d28b3d05c0ba
drstonebraker <drew.stonebraker@gmail.com>
2017-09-20 16:59:43 -0700

    Write users controller auth actions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,29 @@
 class Api::UsersController < ApplicationController
+  before_action :require_login, only: %i(destroy)
+  before_action :require_logout, only: %i(create)
+  # before_action :require_current_user, only: %i(update)
+
   def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: [@user.errors]
+    end
   end
 
   def update
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
