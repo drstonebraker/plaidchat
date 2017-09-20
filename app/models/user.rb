5a9059849a731c88f2b4d4fa2258c3764abbdd58
drstonebraker <drew.stonebraker@gmail.com>
2017-09-20 16:25:17 -0700

    Write User model auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,61 @@
 #
 
 class User < ApplicationRecord
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validate :good_password
+
+  def strong_password
+    unless good_password?
+      errors.add(:password, "can't be things like <em>password<em>,
+        <em>123456<em>, or <em>abcdef<em>")
+    end
+  end
+
+  def strong_password?
+    if ('a'..'z').to_a.concat(('A'..'Z').to_a, (0..9).to_a).includes?(@password)
+      return false
+    end
+    true
+  end
+
+  attr_reader :password
+  after_initialize :ensure_session_token!
+
+  def self.find_by_credentials(username, password)
+    user = self.find_by(username: username)
+    return nil unless user
+    user.valid_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    token = SecureRandom.urlsafe_base64
+    while User.exists?(session_token: token)
+      token = SecureRandom.urlsafe_base64
+    end
+    token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token!
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def valid_password?(password)
+    BCrypt.new(self.password_digest).is_password?(password)
+  end
+
 end
