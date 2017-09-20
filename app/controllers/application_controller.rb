733e64fcb57cd4b4a9f5304505bf4ffd405730ef
drstonebraker <drew.stonebraker@gmail.com>
2017-09-20 16:33:06 -0700

    Write application controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def require_login
+    render json: ['You must be logged in'], status: 401 unless logged_in?
+  end
+
 end
