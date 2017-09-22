b03e50b6a7beab385611f1e2875e1fe9b9bcf076
drstonebraker <drew.stonebraker@gmail.com>
2017-09-22 16:23:44 -0700

    Add content styling to modal

--- frontend/components/chat/sidenav/sidenav_header_modal.jsx
+++ frontend/components/chat/sidenav/sidenav_header_modal.jsx
@@ -10,7 +10,17 @@ class SidenavHeaderModal extends React.Component {
         backgroundColor: 'transparent',
       },
       content: {
-
+        position: 'fixed',
+        zIndex: 10,
+        top: '5.7rem',
+        left: '1rem',
+        bottom: '4.3rem',
+        width: '28.5rem',
+        border: 'none',
+        borderRadius: '0.5rem',
+        overflowY: 'scroll',
+        boxShadow: '0 0 0 0.1rem rgba(0, 0, 0, .1),' +
+          ' 0 0.5rem 1.4rem rgba(10, 10, 10, .2)'
       }
     }
   }
