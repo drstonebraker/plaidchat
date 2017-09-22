78eddff8e05c2fc9306c28b6a0804cc2f70403bf
drstonebraker <drew.stonebraker@gmail.com>
2017-09-21 20:14:01 -0700

    Add react component template files

---
+++ frontend/components/class_template.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+//
+// import XXX from 'XXX'
+//
+// class XXX extends React.component {
+//   constructor(props) {
+//     super(props)
+//   }
+//
+//   componentDidMount() {
+//
+//   }
+//
+//   render() {
+//
+//     return (
+//       <div>
+//
+//       </div>
+//     )
+//   }
+// }
+//
+// export default XXX
---
+++ frontend/components/container_template.jsx
@@ -0,0 +1,22 @@
+// import { connect } from 'react-redux'
+//
+// import  from ''
+//
+// const mapStateToProps = (state, ownProps) => {
+//
+//   return {
+//
+//   };
+// };
+//
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//
+//   return {
+//
+//   };
+// };
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )();
