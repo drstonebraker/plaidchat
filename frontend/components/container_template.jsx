7616ac6de83da286690d0d8f5e3d706873501ecc
drstonebraker <drew.stonebraker@gmail.com>
2017-09-21 20:14:53 -0700

    Modify container template

--- frontend/components/container_template.jsx
+++ frontend/components/container_template.jsx
@@ -1,6 +1,6 @@
 // import { connect } from 'react-redux'
 //
-// import  from ''
+// import XXX from 'XXX'
 //
 // const mapStateToProps = (state, ownProps) => {
 //
@@ -19,4 +19,4 @@
 // export default connect(
 //   mapStateToProps,
 //   mapDispatchToProps
-// )();
+// )(XXX);
