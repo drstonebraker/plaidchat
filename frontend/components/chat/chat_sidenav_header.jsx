eed555d017bf1cb8e5a6a95ca9af930cbd655560
drstonebraker <drew.stonebraker@gmail.com>
2017-09-22 11:28:16 -0700

    Finish styling chat sidenav header

--- frontend/components/chat/chat_sidenav_header.jsx
+++ frontend/components/chat/chat_sidenav_header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import StatusIndicator from './status_indicator'
 import { CaretDown } from '../font_awesome/icons'
 
 class ChatSidenavHeader extends React.Component {
@@ -16,16 +17,20 @@ class ChatSidenavHeader extends React.Component {
 
     return (
       <div className='chat_sidenav_header'>
-        <div>
-          <h1 className='chat_sidenav_header__heading'>
+        <div className='chat_sidenav_header__heading'>
+          <h1 className='chat_sidenav_header__teamname'>
             { teamName }
           </h1>
           <CaretDown color='grey' size='1.6rem' />
         </div>
 
-        <h4 className='chat_sidenav_header__username'>
-          { currentUser.username }
-        </h4>
+        <div>
+          <StatusIndicator />
+          <h4 className='chat_sidenav_header__username'>
+            { currentUser.username }
+          </h4>
+        </div>
+
       </div>
     )
   }
