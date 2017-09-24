c5a4f0906c9b62e939f7790abe8ab12dd538483b
drstonebraker <drew.stonebraker@gmail.com>
2017-09-23 17:50:32 -0700

    Create new team form component skeletons

---
+++ frontend/components/chatgroup_forms/new_chatgroup_form_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'
+
+import NewChatgroupForm from './new_chatgroup_form'
+
+const mapStateToProps = (state, ownProps) => {
+
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+
+  return {
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NewChatgroupForm);
