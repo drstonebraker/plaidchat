11fedb51625ed21b3a97aa6c2fcc1556a09327ab
drstonebraker <drew.stonebraker@gmail.com>
2017-09-24 16:25:48 -0700

    Write channel name custom validation

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -14,7 +14,21 @@ class Channel < ApplicationRecord
   validates :name, :is_direct_message, presence: true
   validates :name, uniqueness: { scope: :team_id,
     message: 'cannot have the same name as another channel on this team' }
+  validate :valid_name
 
   belongs_to :team
+
+  def valid_name
+    unless valid_name?
+      errors.add(:name, "must be lowercase, without spaces or periods," \
+        " and shorter than 22 Characters.")
+    end
+  end
+
+  def valid_name?
+    return false if @name =~ /[A-Z\s\.]/
+    return false if @name.length >= 22
+    true
+  end
 
 end
