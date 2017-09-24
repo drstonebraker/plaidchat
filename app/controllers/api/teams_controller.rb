ad506fc22ea9595fa6a21098f274fd5c62909f1d
drstonebraker <drew.stonebraker@gmail.com>
2017-09-24 16:52:17 -0700

    Update teams controller to create default channels

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -5,14 +5,12 @@ class Api::TeamsController < ApplicationController
 
   def create
     @team = Team.new(team_params)
-    # create membership for creator
-    @team.user_ids = current_user.id
-
-    # TODO: create standard channels
 
     if @team.save
-      # set as default team
-      current_user.update!(default_team_id: @team.id)
+      create_membership(@team)
+      set_default_team(@team)
+      create_default_channels(@team)
+
       render :show
     else
       @errors = [@team.errors.messages]
@@ -25,6 +23,32 @@ class Api::TeamsController < ApplicationController
 
   def team_params
     params.require(:team).permit(:name)
+  end
+
+  def create_default_channels(team)
+    general_channel = Channel.create!(
+      name: 'general',
+      team_id: team.id,
+    )
+
+    Channel.create!(
+      name: 'random',
+      team_id: team.id,
+    )
+
+    TeamMembership.create!(
+      team_id: team.id,
+      user_id: current_user.id,
+      default_channel_id: general_channel.id
+    )
+  end
+
+  def create_membership(team)
+    team.user_ids = [current_user.id]
+  end
+
+  def set_default_team(team)
+    current_user.update!(default_team_id: team.id)
   end
 
 end
