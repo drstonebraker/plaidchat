daa8e8727c51ee1307338b7eb60651c79a131a09
drstonebraker <drew.stonebraker@gmail.com>
2017-09-24 21:33:05 -0700

    Update team membership reducer

--- frontend/reducers/team_membership_entities_reducer.js
+++ frontend/reducers/team_membership_entities_reducer.js
@@ -5,17 +5,24 @@ import {
 import {
   RECEIVE_TEAM,
 } from '../actions/team_actions';
+import {
+  RECEIVE_TEAM_MEMBERSHIP,
+} from '../actions/team_membership_actions';
 
 const teamMembershipEntitiesReducer = (state = {}, action) => {
   Object.freeze(state);
   const newState = Object.assign({}, state)
   const teamMemberships = action.teamMemberships
+  const teamMembership = action.teamMembership
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return arrayToObj(action.teamMemberships);
     case RECEIVE_TEAM:
       return Object.assign(newState, arrayToObj(teamMemberships))
+    case RECEIVE_TEAM_MEMBERSHIP:
+      newState[teamMembership.id] = teamMembership
+      return newState
     default:
       return state;
   }
