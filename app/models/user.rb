142377fb8cbaa759dcb9f012f77f2ea7be2267d2
drstonebraker <drew.stonebraker@gmail.com>
2017-09-25 09:19:58 -0700

    Change default team to default team membership

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  username        :string           not null
-#  password_digest :string           not null
-#  session_token   :string           not null
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  default_team_id :integer          not null
+#  id                         :integer          not null, primary key
+#  username                   :string           not null
+#  password_digest            :string           not null
+#  session_token              :string           not null
+#  created_at                 :datetime         not null
+#  updated_at                 :datetime         not null
+#  default_team_membership_id :integer          not null
 #
 
 class User < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170924234206) do
+ActiveRecord::Schema.define(version: 20170925161542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20170924234206) do
     t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "default_team_id", null: false
+    t.integer "default_team_membership_id", null: false
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username", "password_digest"], name: "index_users_on_username_and_password_digest", unique: true
   end
