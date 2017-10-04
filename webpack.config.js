498150386e5e0d43b35134f99923245ff452c0e4
drstonebraker <drew.stonebraker@gmail.com>
2017-10-03 17:19:23 -0700

    Update webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
 var path = require('path');
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
+  context: __dirname,
   entry: './frontend/entry.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
@@ -18,9 +40,9 @@ module.exports = {
       }
     ]
   },
+  plugins: plugins,
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
-  },
-  watch: true
+  }
 };
