0da6b7fd20305dc9715ead52cfae793d65e6da0f
drstonebraker <drew.stonebraker@gmail.com>
2017-10-30 21:37:03 -0700

    Follow online guide for configuring redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://plaidchat.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://plaidchat.herokuapp.com', 'http://plaidchat.herokuapp.com']
 end
