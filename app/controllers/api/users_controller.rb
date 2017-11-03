289220ca6e332f9e1ae45bf978e6f73b08d61b7c
drstonebraker <drew.stonebraker@gmail.com>
2017-11-02 23:08:46 -0700

    Add is_demo to user_params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -66,7 +66,7 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :default_team_membership_id)
+    params.require(:user).permit(:username, :password, :default_team_membership_id, :is_demo)
   end
 
 end
