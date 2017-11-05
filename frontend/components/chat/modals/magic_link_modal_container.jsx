987ee77760bf8c7d80347f7ca028471b05270a3b
drstonebraker <drew.stonebraker@gmail.com>
2017-11-05 00:56:14 -0700

    Create magic link modal files

---
+++ frontend/components/chat/modals/magic_link_modal_container.jsx
@@ -0,0 +1,22 @@
+// import { connect } from 'react-redux'
+//
+// import MagicLinkModal from './magic_link_modal'
+//
+// const mapStateToProps = (state, ownProps) => {
+//
+//   return {
+//
+//   };
+// };
+//
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//
+//   return {
+//
+//   };
+// };
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(MagicLinkModal);
